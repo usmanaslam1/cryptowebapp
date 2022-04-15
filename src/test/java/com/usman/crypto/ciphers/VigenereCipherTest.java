badd63f8151e6a271c8ebbde77e6ad2374821a20
usman <Usman Aslam>
2022-04-15 01:19:39 -0700

    sdgasdg

--- src/test/java/com/usman/crypto/ciphers/VigenereCipherTest.java
+++ src/test/java/com/usman/crypto/ciphers/VigenereCipherTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class VigenereCipherTest {
 
-
+	//Test to check if Vigenere Key is correctly created
 	@Test
 	public void testVigenereKey() {
 		String key="helloWorld";
@@ -15,12 +15,63 @@ public class VigenereCipherTest {
 		VigenereCipher vc=new VigenereCipher();
 		String e=vc.encrypt(plainText, key);
 		String k=vc.getKey();
-		assertTrue(false);
-
-		//assertTrue("Malformed vigenere key", k.equals("HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDH"));
+		assertTrue("Malformed vigenere key", k.equals("HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDH"));
 		
 	}
 	
+	//Test to verify that non-alpha characters in key are rejected
+
+/*	@Test
+	public void testVigenereKeyChars() {
+		
+		//Data 
+		String key="hello&#%World";
+		String plainText="A quick brown fox jumps over the lazy dog";
+		
+		//Run test
+		VigenereCipher vc=new VigenereCipher();
+		String e=vc.encrypt(plainText, key);
+		String k=vc.getKey();
+		
+		//Report result
+		assertTrue("Malformed vigenere key", k.equals("HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDH"));
+		
+	}
 	
+	//Test to verify that for a given plaintext and key, correct ciphertext is created
+	@Test
+	public void testEncryption() {
+		String key="helloWorld";
+		String plainText="A quick brown fox jumps over the lazy dog";
+		VigenereCipher vc=new VigenereCipher();
+		String e=vc.encrypt(plainText, key);
+		String k=vc.getKey();
+		assertTrue("Malformed vigenere key", k.equals("HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDH"));
+		
+	}
+	
+	//Test to verify that for a given plain text and key, correct cipher text is created
+	@Test
+	public void testDecryption() {
+		String key="helloWorld";
+		String plainText="A quick brown fox jumps over the lazy dog";
+		VigenereCipher vc=new VigenereCipher();
+		String e=vc.encrypt(plainText, key);
+		String k=vc.getKey();
+		assertTrue("Malformed vigenere key", k.equals("HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDH"));
+		
+	}
+	
+	//Test to verify that non-alpha characters in plaintext are ignored and not encrypted
+	@Test
+	public void testInvalidValues() {
+		String key=null;
+		String plainText=null;
+		VigenereCipher vc=new VigenereCipher();
+		String e=vc.encrypt(plainText, key);
+		String k=vc.getKey();
+		assertTrue("Malformed vigenere key", k.equals("HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDH"));
+		
+	}	*/
 }
 
